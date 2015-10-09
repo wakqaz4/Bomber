9965652ec51912b75baf0ffff8c7b0ae37572ac2
qiuhao <2454664898@qq.com>
2015-10-09 14:22:30 +0800

    删掉注释

--- QiuHao/Classes/Layer/UILayer.h
+++ QiuHao/Classes/Layer/UILayer.h
@@ -17,11 +17,7 @@ public:
 	void DirSliderEvent(cocos2d::Ref *pSender, cocos2d::ui::Slider::EventType type);
 	void PowerSliderEvent(cocos2d::Ref *pSender, cocos2d::ui::Slider::EventType type);
 	
-	/*
-	内联函数的定义对编译器必须是可见的, 这样编译器才可以在调用点进行内联展开该函数的代码.
-	如果提供的头文件中,只有函数原型, 显然是不够的, 要把inline函数的函数体也放进头文件.
-	inline的替换是在编译的时候就被执行的(到不了链接这步), 类似于宏,
-	*/
+
 	inline unsigned int getDirValue() { return this->dirValue; }
 	inline void setDirValue(int dirValue){ this->dirValue = dirValue; }
 	inline unsigned int getPowerValue(){ return this->powerValue; }
